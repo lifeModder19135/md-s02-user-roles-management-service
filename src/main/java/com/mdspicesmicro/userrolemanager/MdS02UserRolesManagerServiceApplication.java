c78897ffe0c5544dfc6bd0d497e552819c39340e
Nate Tolbert <ntolbertu85@gmail.com>
2020-11-27 22:16:54 -0500

    11272020 -- project-wide-scope -- INITIAL COMMIT;

---
+++ src/main/java/com/mdspicesmicro/userrolemanager/MdS02UserRolesManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mdspicesmicro.userrolemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdS02UserRolesManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdS02UserRolesManagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdspicesmicro/userrolemanager/MdS02UserRolesManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdspicesmicro.userrolemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdS02UserRolesManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
